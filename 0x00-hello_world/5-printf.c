c06e05a0d951493141fc2635a4467f4532bdc952
MinasA1 <minasanton@gmail.com>
2017-05-11 02:21:18 +0000

    added descriptions

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point
+ * display message onscreen
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 printf("%s\n", "with proper grammar, but the outcome is a piece of art,");
