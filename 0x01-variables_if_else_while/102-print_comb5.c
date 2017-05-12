f6109a3ab04980243a474b41ad6bffd3bb818e2d
MinasA1 <minasanton@gmail.com>
2017-05-12 04:34:39 +0000

    advanced corrected

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,10 +12,10 @@ for (i = 0; i < 99; i++)
 {
 for (j = i + 1; j < 100; j++)
 {
-putchar('0' + (i - i % 10) / 10);
-putchar('0' + i % 10);
+putchar(i / 10 + '0');
+putchar(i % 10 + '0');
 putchar(' ');
-putchar('0' + (j - j % 10) / 10);
+putchar('0' + j / 10);
 putchar('0' + j % 10);
 
 if (i != 98 || j != 99)
