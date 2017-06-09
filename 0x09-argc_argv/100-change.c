689e42e835d7572d16742500d68bdb6f7a4a356a
MinasA1 <minasanton@gmail.com>
2017-06-09 04:15:51 +0000

    Advanced added

---
+++ 0x09-argc_argv/100-change.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - prints minimum number of coins to give changes.
+ *@argc: counts arguments
+ *@argv: stores arguments in a pointer of arrays
+ *Return: 0 if no errors, else 1
+ */
+int main(int argc, char *argv[])
+{
+	int m, ch = 0, i, s;
+	int c[5] = {25, 10, 5, 2, 1};
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	m = atoi(argv[1]);
+	if (m < 0)
+	{
+		printf("0\n");
+	}
+	else
+	{
+		for (i = 0; i < 5; i++)
+		{
+			s = m / c[i];
+			m -= s * c[i];
+			ch += s;
+			if (m == 0)
+				break;
+		}
+	}
+	printf("%d\n", n);
+	return (0);
+}
