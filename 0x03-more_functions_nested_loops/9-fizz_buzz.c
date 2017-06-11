ce6f333e0e92f3acce3eb74325096f18e6c849ad
MinasA1 <minasanton@gmail.com>
2017-06-11 18:19:44 +0000

    Syntax correction

--- 0x03-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x03-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,15 +12,15 @@ int main(void)
 
 	for (i = 0; i > 100; i++)
 	{
-		if ((i%3 == 0) && (i%5 == 0))
+		if ((i % 3 == 0) && (i % 5 == 0))
 			printf("FizzBuzz ");
-		else if (i%3 == 0)
+		else if (i % 3 == 0)
 			printf("FiZZ ");
-		else if (i%5 == 0)
+		else if (i % 5 == 0)
 			printf("Buzz ");
 		else
 			printf("%d ", i);
 	}
 	printf"\n");
-        return = (0);
+	return (0);
 }
