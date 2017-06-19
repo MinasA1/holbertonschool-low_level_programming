7f956dd875d47a8c4adb2681aee8f2849501d9b3
Minas Anton <minasanton@gmail.com>
2017-06-19 11:56:53 -0700

    fixed loop

--- 0x0F-variadic_functions/1-print_numbers.c
+++ 0x0F-variadic_functions/1-print_numbers.c
@@ -15,7 +15,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 
 	va_start(numbers, n);
-	for (i = 0; i < n - 1; i++)
+	for (i = 1; i < n; i++)
 	{
 		printf("%d, ", va_arg(numbers, int));
 		if (separator != NULL)
