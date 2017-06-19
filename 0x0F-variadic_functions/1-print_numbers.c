0fb78b0c37027d9d945aed9897718dbd70889af6
Minas Anton <minasanton@gmail.com>
2017-06-19 11:53:48 -0700

    fixed separator

--- 0x0F-variadic_functions/1-print_numbers.c
+++ 0x0F-variadic_functions/1-print_numbers.c
@@ -14,11 +14,13 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list numbers;
 	unsigned int i;
 
-	if (separator != NULL)
-		printf("%s", separator);
 	va_start(numbers, n);
 	for (i = 0; i < n - 1; i++)
+	{
 		printf("%d, ", va_arg(numbers, int));
+		if (separator != NULL)
+			printf("%s", separator);
+	}
 	printf("%d\n", va_arg(numbers, int));
 	va_end(numbers);
 	exit(EXIT_SUCCESS);
