82a6756014740a8d98f8544fe75492e4867bd919
MinasA1 <minasanton@gmail.com>
2017-06-21 04:08:11 +0000

    edge case added

--- 0x0F-variadic_functions/1-print_numbers.c
+++ 0x0F-variadic_functions/1-print_numbers.c
@@ -1,6 +1,7 @@
 #include <stdarg.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <limits.h>
 /**
  * print_numbers - returns the sum of all its parameters.
  * @separator: pointer to string to be printed
@@ -13,13 +14,18 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list numbers;
 	unsigned int i;
+	int a;
 
 	va_start(numbers, n);
 	for (i = 1; i < n; i++)
 	{
-		printf("%d", va_arg(numbers, int));
-		if (separator != NULL)
-			printf("%s", separator);
+		a = va_arg(numbers, int);
+		if (a <= INT_MAX && a >= INT_MIN)
+		{
+			printf("%d", a);
+			if (separator != NULL)
+				printf("%s", separator);
+		}
 	}
 	printf("%d\n", va_arg(numbers, int));
 	va_end(numbers);
