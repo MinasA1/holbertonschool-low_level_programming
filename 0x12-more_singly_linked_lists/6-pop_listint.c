c0ee9e559e2ca8f3682e4cc209f1bb897b9037f0
MinasA1 <minasanton@gmail.com>
2017-06-28 18:03:00 +0000

    space fixed

--- 0x12-more_singly_linked_lists/6-pop_listint.c
+++ 0x12-more_singly_linked_lists/6-pop_listint.c
@@ -1,9 +1,11 @@
-#include"lists.h"
+#include "lists.h"
+
 /**
  * pop_listint - deletes the head node of a linked list
  * @head: pointer to head
  * Return: data of node
  */
+
 int pop_listint(listint_t **head)
 {
 	listint_t *temp = *head;
