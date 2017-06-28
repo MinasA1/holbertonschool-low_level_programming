e617d4ba2fc1ae8e5aba5f3c082a0a4d8b24ed8e
Minas Anton <minasanton@gmail.com>
2017-06-28 10:44:45 -0700

    fixed NULL case

--- 0x12-more_singly_linked_lists/5-free_listint2.c
+++ 0x12-more_singly_linked_lists/5-free_listint2.c
@@ -8,12 +8,13 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
+	if (head == NULL || *head == NULL)
+			return;
 	while (*head)
 	{
 		temp = *head;
-		*head = (*head)->next;
-		free(temp);
+		*head = *head->next;
+		free(temp)';
 	}
-	*head = NULL;
 
 }
