69d579d24b2bddaba286e6e17124d07953b0537b
MinasA1 <minasanton@gmail.com>
2017-06-28 20:49:03 +0000

    fixed edge cases

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,8 +16,13 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 	new->n = n;
 	if (*head == NULL && idx == 0)
+	{
 		new->next = NULL;
-	else if(*head == NULL && idx)
+		*head = new;
+		return (new);
+
+	}
+	else if(*head == NULL && idx != 0)
 		return (NULL);
 	temp = *head;
 	for (i = 0; i < idx - 1; i++)
