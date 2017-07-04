e688395aa171b13c5b4d59c1f735985dce5607d7
MinasA1 <minasanton@gmail.com>
2017-07-04 06:01:05 +0000

    corrected name of argument vars

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -3,8 +3,8 @@
 
 /**
  * main - copies the content of a file to another file
- * @argc: argument counter
- * @argv: pointer cointaining args passed in
+ * @ac: argument counter
+ * @av: pointer cointaining args passed in
  * Return: Always 0.
  */
 int main(int ac, char **av)
@@ -17,16 +17,16 @@ int main(int ac, char **av)
 		write(2, "Usage: cp file_from file_to\n", 29);
 		exit(97);
 	}
-	fdin = open(argv[1], O_RDONLY);
+	fdin = open(av[1], O_RDONLY);
 	if (fdin == -1)
 	{
-		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+		dprintf(2, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
-	fdout = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	fdout = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fdout == -1)
 	{
-		dprintf(2, "Error: Can't write to %s\n", argv[2]);
+		dprintf(2, "Error: Can't write to %s\n", av[2]);
 		exit(99);
 	}
 	in = read(fdin, buff, 1024);
@@ -34,13 +34,13 @@ int main(int ac, char **av)
 	{
 		if (in == -1)
 		{
-			dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+			dprintf(2, "Error: Can't read from file %s\n", av[1]);
 			exit(98);
 		}
 		out = write(fdout, buff, in);
 		if (out == -1)
 		{
-			dprintf(2, "Error: Can't write to %s\n", argv[2]);
+			dprintf(2, "Error: Can't write to %s\n", av[2]);
 			exit(99);
 		}
 		in = read(fdin, buff, 1024);
