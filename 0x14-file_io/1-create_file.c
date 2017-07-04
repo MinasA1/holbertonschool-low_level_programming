4d1c837f34952f7e0d4c0a392d955e018b4e47fd
MinasA1 <minasanton@gmail.com>
2017-07-04 04:42:28 +0000

    cheanged fd permissions

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -13,9 +13,9 @@ int create_file(const char *filename, char *text_content)
 
 	while (text_content[i])
 		i++;
-	if (filename == NULL || filename[0] == '\0')
+	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR11);
 	if (fd == -1)
 	{
 		close(fd);
