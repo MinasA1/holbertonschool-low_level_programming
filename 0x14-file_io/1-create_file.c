dbc430a5ef9277ce48570fccf6e6423490475d8d
Minas Anton <minasanton@gmail.com>
2017-07-03 23:43:11 -0700

    correcting betty

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -6,7 +6,6 @@
  * @text_content: text to add in file
  * Return: returns 1 on succesm , -1 on failure
  */
-
 int create_file(const char *filename, char *text_content)
 {
 	int c, fd, i = 0;
@@ -15,9 +14,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
-	{
 		return (-1);
-	}
 	if (text_content == NULL || text_content[0] == '\0')
 	{
 		close(fd);
