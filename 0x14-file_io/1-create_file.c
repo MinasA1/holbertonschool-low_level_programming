c0c70601dfea534133b9f4baddf3cd0fed689ac0
MinasA1 <minasanton@gmail.com>
2017-07-04 04:47:10 +0000

    closing and returning fixes

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -18,11 +18,13 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR11);
 	if (fd == -1)
 	{
-		close(fd);
 		return (-1);
 	}
 	if (text_content == NULL || text_content[0] == '\0')
+	{
+		close(fd);
 		return (1);
+	}
 	c = write(fd, text_content, i);
 	if (c == -1)
 	{
