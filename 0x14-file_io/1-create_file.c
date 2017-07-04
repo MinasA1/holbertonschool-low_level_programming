42f9ea20a2525538580cabeacfe0b31b48300f5d
MinasA1 <minasanton@gmail.com>
2017-07-04 04:37:55 +0000

    betty fixes

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -15,7 +15,7 @@ int create_file(const char *filename, char *text_content)
 		i++;
 	if (filename == NULL || filename[0] == '\0')
 		return (-1);
-	fd = creat(filename, S_IRUSR | S_IWUSR);
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
 	{
 		close(fd);
