00fcbc3983832ce187cfa51a40267ab370ca8cb3
MinasA1 <minasanton@gmail.com>
2017-07-06 19:40:30 +0000

    betty and output fixes

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -14,28 +14,19 @@ int main(int ac, char **av)
 	mode_t perm = (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
 
 	if (ac != 3)
-	{
-		write(2, "Usage: cp file_from file_to\n", 29);
-		exit(97);
-	}
+		write(STDERR_FILENO, "Usage: cp file_from file_to\n", 28), exit(97);
 	fdin = open(av[1], O_RDONLY);
 	if (fdin == -1)
-	{
-		dprintf(2, "Error: Can't read from file %s\n", av[1]);
-		exit(98);
-	}
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
 	fdout = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, perm);
 	if (fdout == -1)
-	{
-		dprintf(2, "Error: Can't write to %s\n", av[2]);
-		exit(99);
-	}
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
 	in = read(fdin, buff, 1024);
 	while (in)
 	{
 		if (in == -1)
 		{
-			dprintf(2, "Error: Can't read from file %s\n", av[1]);
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 			exit(98);
 		}
 		out = write(fdout, buff, in);
@@ -48,10 +39,7 @@ int main(int ac, char **av)
 	}
 	in = close(fdin);
 	if (in == -1)
-	{
-		dprintf(2, "Error: Can't close fd %d\n", fdin);
-		exit(100);
-	}
+		dprintf(2, "Error: Can't close fd %d\n", fdin), exit(100);
 	out = close(fdout);
 	if (out == -1)
 	{
