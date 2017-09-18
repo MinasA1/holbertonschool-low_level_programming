4126ac01e22f9b54a02724343ec271855a0dfc97
MinasA1 <minsanton@gmail.com>
2017-09-18 20:55:59 +0000

    changed check and loop condition

--- 0x1A-sorting_algorithms/0-bubble_sort.c
+++ 0x1A-sorting_algorithms/0-bubble_sort.c
@@ -10,19 +10,20 @@ void bubble_sort(int *array, size_t size)
 	size_t i, j;
 	int temp;
 
-	if (array == NULL || size == 0)
-		return;
-	for (i = 0; i < size; i++)
-	{
+	if (array && size > 0)
+	  {
+	    for (i = 0; i < size - 1; i++)
+	      {
 		for (j = 0; j < (size - i - 1); j++)
-		{
-			if (array[j] > array[j + 1])
-			{
-				temp = array[j];
-				array[j] = array[j + 1];
-				array[j + 1] = temp;
-				print_array(array, size);
-			}
-		}
-	}
+		  {
+		    if (array[j] > array[j + 1])
+		      {
+			temp = array[j];
+			array[j] = array[j + 1];
+			array[j + 1] = temp;
+			print_array(array, size);
+		      }
+		  }
+	      }
+	  }
 }
