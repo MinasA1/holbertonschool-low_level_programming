7df1dc88cb36f5713feb1635033753f8b1f1de15
MinasA1 <minsanton@gmail.com>
2017-09-18 21:30:40 +0000

    added null check

--- 0x1A-sorting_algorithms/2-selection_sort.c
+++ 0x1A-sorting_algorithms/2-selection_sort.c
@@ -10,20 +10,23 @@ void selection_sort(int *array, size_t size)
 	size_t i, j, k;
 	int a;
 
-	for (i = 0; i < size - 1; i++)
-	{
+	if (array && size > 0)
+	  {
+	    for (i = 0; i < size - 1; i++)
+	      {
 		k = i;
 		for (j = i + 1; j < size; j++)
-		{
-			if (array[j] < array[k])
-				k = j;
-		}
+		  {
+		    if (array[j] < array[k])
+		      k = j;
+		  }
 		if (k != i)
-		{
-			a = array[i];
-			array[i] = array[k];
-			array[k] = a;
-			print_array(array, size);
-		}
-	}
+		  {
+		    a = array[i];
+		    array[i] = array[k];
+		    array[k] = a;
+		    print_array(array, size);
+		  }
+	      }
+	  }
 }
