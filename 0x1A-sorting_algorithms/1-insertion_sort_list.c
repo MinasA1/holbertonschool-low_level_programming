07e948e32f6d8fa248dfd652ef8612764e87c052
MinasA1 <minsanton@gmail.com>
2017-09-20 02:56:31 +0000

    hotfix

--- 0x1A-sorting_algorithms/1-insertion_sort_list.c
+++ 0x1A-sorting_algorithms/1-insertion_sort_list.c
@@ -1,29 +1,29 @@
 #include "sort.h"
-
 /**
- * insertion_sort_list - sorting algorithm
- * @list: linked list to sort
- */
+* insertion_sort_list - sorting algorithm
+* @list: linked list to sort
+*/
 void insertion_sort_list(listint_t **list)
 {
 	listint_t *temp;
 	int a;
 
-	if (list == NULL)
-		return;
 	temp = *list;
 	while (temp)
 	{
-		if (temp->next)
+		while (temp)
 		{
-			if (temp->n > temp->next->n)
+			if (temp->next)
 			{
-				a = temp->n;
-				*(int *)&temp->n = temp->next->n;
-				*(int *)&temp->next->n = a;
-				temp = *list;
-				print_list(*list);
-				continue;
+				if (temp->n > temp->next->n)
+				{
+					a = temp->n;
+					*(int *)&temp->n = temp->next->n;
+					*(int *)&temp->next->n = a;
+					temp = *list;
+					print_list(*list);
+					break;
+				}
 			}
 			temp = temp->next;
 		}
