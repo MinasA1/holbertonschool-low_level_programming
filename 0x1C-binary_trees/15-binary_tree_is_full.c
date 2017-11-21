4ba1ad14206a717eaffe98dbfe1c90513894a26c
Minas <minasanton@gmail.com>
2017-11-21 02:34:04 +0000

    fixed betty

--- 0x1C-binary_trees/15-binary_tree_is_full.c
+++ 0x1C-binary_trees/15-binary_tree_is_full.c
@@ -6,15 +6,15 @@
  */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-  int left = 0, right = 0;
+	int left = 0, right = 0;
   
-  if (tree)
-    {      
-      if (!tree->left && !tree->right)
-	return (1);
-      left = binary_tree_is_full(tree->left);
-      right = binary_tree_is_full(tree->right);
-      return (left & right);
-    }
-  return (0);
+	if (tree)
+		{      
+		if (!tree->left && !tree->right)
+			return (1);
+		left = binary_tree_is_full(tree->left);
+		right = binary_tree_is_full(tree->right);
+		return (left & right);
+		}
+	return (0);
 }
