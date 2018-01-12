1b4865a7d725c08a19ccb7329ac1a946f3cc7228
Minas <minasanton@gmail.com>
2018-01-12 08:09:56 +0000

    corrected and optimized algorithms

--- 0x1D-search_algorithms/0-linear.c
+++ 0x1D-search_algorithms/0-linear.c
@@ -9,17 +9,16 @@
 
 int linear_search(int *array, size_t size, int value)
 {
-	int i;
-	(void) size;
+	size_t i;
 
 	if (array)
 	{
-		for (i = 0; array[i]; i++)
-			{
-				printf("Value checked array[%d] = [%d]\n", i , array[i]);
-				if (array[i] == value)
-					return (i);
-			}
+		for (i = 0; i < size; i++)
+		{
+			printf("Value checked array[%lu] = [%d]\n", i , array[i]);
+			if (array[i] == value)
+				return (i);
+		}
 	}
 	return (-1);
 }
--- 0x1D-search_algorithms/1-binary.c
+++ 0x1D-search_algorithms/1-binary.c
@@ -1,39 +1,5 @@
 #include "search_algos.h"
-/**
- * _bsearch - helper function for binary search
- * @array: array to search in
- * @value: value to search for
- * @low: starting index
- * @high: ending index
- */
-int _bsearch(int *array, size_t low, size_t high, int value)
-{
-	size_t i, mid;
 
-	mid = (low + high) / 2;
-
-	printf("Searching in array:");
-	for (i = low; i < high; i++)
-		{
-			printf(" %lu", i);
-			if (i == high - 1)
-				printf("\n");
-			else
-				printf(",");
-		}
-	if (high - low == 1 && value > array[high])
-		return (-1);
-	if (high >= low)
-		{
-		if (value == array[mid])
-			return (mid);
-		else if (value < array[mid])
-			return _bsearch(array, 0, mid, value);
-		else if (value > array[mid])
-			return _bsearch(array, mid, high, value);
-		}
-	return (-1);
-}
 /**
  * binary_search - binary search algorithm
  * @array: array to search in
@@ -43,25 +9,22 @@ int _bsearch(int *array, size_t low, size_t high, int value)
  */
 int binary_search(int *array, size_t size, int value)
 {
-	size_t i , mid;
+	int j;
+	size_t mid, i;
 
-	mid = size / 2;
-
-	printf("Searching in array:");
+	mid = (size - 1) / 2;
+	if (!array || !size)
+		return (-1);
+	printf("Searching in array: ");
 	for (i = 0; i < size; i++)
-		{
-			printf(" %lu", i);
-			if (i == size - 1)
-				printf("\n");
-			else
-				printf(",");
-		}
+		printf("%d%c", array[i], (i < (size - 1)) ? ',' : '\n');
 	if (value == array[mid])
 		return (mid);
-	else if	(value > array[mid])
-		return _bsearch(array, mid, size, value);
-	else
-		return _bsearch(array, 0, mid, value);
-
-	return (-1);
+	mid++;
+	if (value < array[mid])
+		return (binary_search(array, mid, value));
+	j = binary_search(array + mid, size - mid, value);
+	if (j == -1)
+		return (-1);
+	return (mid + j);
 }
