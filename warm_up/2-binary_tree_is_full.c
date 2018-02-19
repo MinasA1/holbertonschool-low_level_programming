1bd66f086f137e33294b6772acf990691bc078f9
kabetanio <minasanton@gmail.com>
2018-02-19 13:56:11 -0800

    Betty Fix

--- warm_up/2-binary_tree_is_full.c
+++ warm_up/2-binary_tree_is_full.c
@@ -7,9 +7,9 @@
 int binary_tree_is_full(const binary_tree_t *tree)
 {
 	int left = 0, right = 0;
-  
+
 	if (tree)
-		{      
+		{
 		if (!tree->left && !tree->right)
 			return (1);
 		left = binary_tree_is_full(tree->left);
